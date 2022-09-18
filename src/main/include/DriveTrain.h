ebfcfaf58b71619fa14b2b032d353f862872ea53
p-328 <69773020+p-328@users.noreply.github.com>
2022-09-18 12:08:27 -0400

    Added TODO in DriveTrain.h

--- src/main/include/DriveTrain.h
+++ src/main/include/DriveTrain.h
@@ -5,6 +5,7 @@
 class DriveTrain {
     private:
         using enum rev::CANSparkMaxLowLevel::MotorType;
+        // TODO: Set motor device ID's to correct ones. (This is when we get the device ID's)
         rev::CANSparkMax u_left{1, kBrushless}; 
         rev::CANSparkMax l_left{2, kBrushless};
         rev::CANSparkMax u_right{3, kBrushless};
