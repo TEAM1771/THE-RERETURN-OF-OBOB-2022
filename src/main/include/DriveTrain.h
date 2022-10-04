5e73421f9235208f6e4b0f0d7f785129df862c3b
Joseph Farkas <16584585+MrRedness@users.noreply.github.com>
2022-10-04 13:08:53 -0400

    Update DriveTrain.h

--- src/main/include/DriveTrain.h
+++ src/main/include/DriveTrain.h
@@ -2,12 +2,6 @@
 
 #include <rev/CanSparkMAX.h>
 
-class DriveTrain {
-    private:
-        rev::CANSparkMax u_left{1, rev::CANSparkMaxLowLevel::MotorType::kBrushless}; 
-        rev::CANSparkMax l_left{2, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
-        rev::CANSparkMax u_right{3, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
-        rev::CANSparkMax l_right{4, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
-    public:
-        void drive(float l, float r);
-};
+namespace DriveTrain {
+    void drive(float l, float r);
+}
