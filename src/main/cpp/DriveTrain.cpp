37d7d910058397a3e5ab793cdf206877f0d60b99
Joseph Farkas <16584585+MrRedness@users.noreply.github.com>
2022-10-04 13:11:40 -0400

    Update DriveTrain.cpp

--- src/main/cpp/DriveTrain.cpp
+++ src/main/cpp/DriveTrain.cpp
@@ -1,5 +1,11 @@
 #include <DriveTrain.h>
 
+static rev::CANSparkMax u_left{1, rev::CANSparkMaxLowLevel::MotorType::kBrushless}; 
+static rev::CANSparkMax l_left{2, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+static rev::CANSparkMax u_right{3, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+static rev::CANSparkMax l_right{4, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+
+
 void DriveTrain::drive(float l, float r) {
     u_left.Set(l);
     u_right.Set(-r);
