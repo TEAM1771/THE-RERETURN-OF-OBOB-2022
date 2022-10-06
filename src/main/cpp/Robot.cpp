aaa8d1b81922666bd8dd58e36a34227225fcfc56
team 1771's personal <ngrobo1771@gmail.com>
2022-10-06 19:33:00 -0400

    print vals to NT

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -6,6 +6,7 @@
 #include "Buttons.hpp"
 #include "DriveTrain.hpp"
 #include "Intake.hpp"
+#include "Navx.hpp"
 
 #include <frc/Joystick.h>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -26,19 +27,13 @@ void Robot::RobotInit()
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic()
+{
+  frc::SmartDashboard::PutNumber("Pitch", Navx::getPitch().value());
+  frc::SmartDashboard::PutNumber("Roll", Navx::getRoll().value());
+  frc::SmartDashboard::PutNumber("Yaw", Navx::getYaw().value());
+}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
 void Robot::AutonomousInit()
 {
   using namespace std::literals::chrono_literals;
@@ -59,7 +54,7 @@ void Robot::TeleopPeriodic()
 
   DriveTrain::drive(l, r);
 
-  if(BUTTON::INTAKE)
+  if (BUTTON::INTAKE)
     Intake::run(false);
   else if (BUTTON::OUTTAKE)
     Intake::run(true);
